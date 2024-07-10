e585f9607ec23d0604d3d797918000ffdff4cadf
dhayac <dhayananthan.c@science.christuniversity.in>
2024-07-10 11:34:52 +0530

    fix: corrected gridfs

--- db_utilites/mongodb.py
+++ db_utilites/mongodb.py
@@ -23,31 +23,34 @@ class MongoDB:
             logger.error(f"Error during pinging error: {e}")
 
 
-    def get_collection(self, name: str):
+    def get_collection(self, collection_name: str, database_name: str):
         try:
-            collection = self.client[name]
-            return collection
+            db = self.client.get_database(database_name)
+
+            collection = db.get_collection(collection_name)
+            return db,collection
         except Exception as e:
             logger.error(f"Error in get Collection {e}")
     
-    def add_files(self,content: str,fileid: str, topic: str, filename: str,collection: Database):
+    def add_files(self,content: str,fileid: str, topic: str, filename: str,collection,db,grid_collection: str):
         try:
-            fs = GridFS(collection)
+            fs = GridFS(db, collection=grid_collection)
             md5 = hashlib.md5()
             md5.update(content)
-            fs.put(content, fileid = fileid)
+            fs_id = fs.put(content, fileid = fileid)
 
             metadata = {
                 "filename": filename,
                 "fileid": fileid,
                 "topic": topic,
-                "hash" : md5.hexdigest()
-            }
-
+                "hash" : md5.hexdigest(),
+                "fs_id" : fs_id}
             collection.insert_one(metadata)
-            logger.info(f"Sucess fully added to database")
+            logger.info("Sucessfully added to collection")
+            return {"message": f"Sucessfully added to collection {filename}"}
         except Exception as exe:
             logger.error(f"Error during adding files to mongoDB: {exe}")
+            return {"error": str(exe)}, 500
 
 
     
