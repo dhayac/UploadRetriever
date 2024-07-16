02a783186c36e903e81002d33be28fc824905c4f
dhayac <dhayananthan.c@science.christuniversity.in>
2024-07-17 00:00:29 +0530

    fix: removed initialized method and added fetch constants

--- app/utilities/faiss_db.py
+++ app/utilities/faiss_db.py
@@ -1,16 +1,16 @@
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
-import numpy as np
 from langchain_core.documents import Document
-import faiss
 from app.utilities import s_logger
-from pydantic import BaseModel
-import os
+from app.utilities.constants import Constants
 
 logger = s_logger.LoggerAdap(s_logger.get_logger(__name__), {"vectordb":"faiss"})
 class FaissDB:
+    
     def __init__(self):
-        self.embedding_name = "D:\\fastapi\\embedding_model\\all-mini-lm-l6-v2"
+        self.embedding_path = Constants.fetch_constant("embedding_model")["path"]
+        self.embeddings = HuggingFaceEmbeddings(model_name=self.embedding_path)
+    
     def __create_vectordb(self):
         with open(r"D:\fastapi\documents\Car.txt",encoding="utf-8") as f:
             self.text = f.read()
@@ -21,11 +21,14 @@ class FaissDB:
         self.db.save_local(r"D:\fastapi\vectorstore")
     
     def load_vectordb(self):
-        self.db = FAISS.load_local(r"app/vectorstore", self.embeddings,allow_dangerous_deserialization=True)
-
+        try:
+            self.db = FAISS.load_local(Constants.fetch_constant("faissdb")["path"], self.embeddings,allow_dangerous_deserialization=True)
+        except Exception as exe:
+            logger.error(f"Error in load_vectordb {exe}")
+    
     def save_local(self):
         try:
-            self.db.save_local(r"app/vectorstore")
+            self.db.save_local(Constants.fetch_constant("faissdb")["path"])
             logger.info("Vector DB saved in local")
         except Exception as exe:
             logger.error(f"Error during save vector in local: message {exe}")
@@ -37,9 +40,6 @@ class FaissDB:
             return message
         except Exception as exe:
             logger.error(f"Error during add document in faissdb {exe}")
-     
-    def initialize(self):
-        self.embeddings = HuggingFaceEmbeddings(model_name=self.embedding_name)
     
     def run_query(self, query: str) :
         results_with_scores = self.db.similarity_search_with_score(query, k=3)
