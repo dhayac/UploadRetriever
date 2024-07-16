cabd44313eea7304979ae75b43f0c3b42f8a4176
dhayac <dhayananthan.c@science.christuniversity.in>
2024-07-16 23:03:44 +0530

    modified:   removed spaces and comments

--- app/utilities/faiss_db.py
+++ app/utilities/faiss_db.py
@@ -11,7 +11,6 @@ logger = s_logger.LoggerAdap(s_logger.get_logger(__name__), {"vectordb":"faiss"}
 class FaissDB:
     def __init__(self):
         self.embedding_name = "D:\\fastapi\\embedding_model\\all-mini-lm-l6-v2"
-        #self.text  = TextLoader(r"D:\fastapi\documents\Car.txt", encoding="utf-8").load()[0].page_content
     def __create_vectordb(self):
         with open(r"D:\fastapi\documents\Car.txt",encoding="utf-8") as f:
             self.text = f.read()
@@ -21,14 +20,11 @@ class FaissDB:
         self.db = FAISS.from_documents([self.document],embedding=self.embeddings)
         self.db.save_local(r"D:\fastapi\vectorstore")
     
-    
     def load_vectordb(self):
         self.db = FAISS.load_local(r"app/vectorstore", self.embeddings,allow_dangerous_deserialization=True)
 
     def save_local(self):
         try:
-            # for file in os.listdir(r"D:\fastapi\vectorstore"):
-            #     os.remove(os.path.join(r"D:\fastapi\vectorstore", file))
             self.db.save_local(r"app/vectorstore")
             logger.info("Vector DB saved in local")
         except Exception as exe:
@@ -41,16 +37,12 @@ class FaissDB:
             return message
         except Exception as exe:
             logger.error(f"Error during add document in faissdb {exe}")
-    
-
-    
+     
     def initialize(self):
         self.embeddings = HuggingFaceEmbeddings(model_name=self.embedding_name)
     
     def run_query(self, query: str) :
-        
         results_with_scores = self.db.similarity_search_with_score(query, k=3)
-
         dic = {}
         for doc, score in results_with_scores:
             if f"{doc.metadata["fileid"]}" not in dic.keys():
