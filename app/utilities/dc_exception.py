fbdb795c9f383971580362b2c9ba1a9cddcea657
dhayac <dhayananthan.c@science.christuniversity.in>
2024-07-21 21:47:29 +0530

    fix:   added dc_exception

---
+++ app/utilities/dc_exception.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod
+import status
+
+class DcException(Exception):
+
+    @abstractmethod
+    def get_code(self):
+        pass
+    
+    @abstractmethod
+    def get_message(self):
+        pass
+
+class FileNotFoundException(DcException):
+    
+    def __init__(self, message: str=None) -> None:
+        self.code = status.HTTP_404_NOT_FOUND
+        if message:
+            self.message = "fileid not found in data base"
+        else:
+            self.message  = message
+    
+    def get_code(self):
+        return self.code
+    
+    def get_message(self) -> str:
+        return self.message
+    
+    def __str__(self) -> str:
+        return self.message
